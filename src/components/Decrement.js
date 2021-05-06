4cf61f9a3482398ac5596fcbf7202029cd120192
Rao Fahad <raofahad046@gmail.com>
2021-05-06 11:50:22 +0500

    added a increment button component

---
+++ src/components/Decrement.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { myContext } from "../myContext";
+
+const Decrement = () => {
+  const { decrementCount } = useContext(myContext);
+  return (
+    <div>
+      <button onClick={() => decrementCount}>Decrement</button>
+    </div>
+  );
+};
+
+export default Decrement;
