a40a88e16d7bd2c10198a204d84c731f3df368a5
Rao Fahad <raofahad046@gmail.com>
2021-05-06 11:51:13 +0500

    added a reset button component

---
+++ src/components/Reset.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { myContext } from "../myContext";
+
+const Reset = () => {
+  const { resetCount } = useContext(myContext);
+  return (
+    <div>
+      <button onClick={() => resetCount}>Reset</button>
+    </div>
+  );
+};
+
+export default Reset;
