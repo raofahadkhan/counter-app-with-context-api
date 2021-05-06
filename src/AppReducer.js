b5668891e50ae080752ae7d5fdd5f8e86a02443d
Rao Fahad <raofahad046@gmail.com>
2021-05-06 11:49:00 +0500

    added a reducer function

---
+++ src/AppReducer.js
@@ -0,0 +1,26 @@
+import { INCREMENT, DECREMENT, RESET } from "./types";
+
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    case RESET:
+      return {
+        ...state,
+        count: 0,
+      };
+    default:
+      return state;
+  }
+};
+export default AppReducer;
