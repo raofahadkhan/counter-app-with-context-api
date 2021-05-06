8080f287e91c5d53e3333b736f30687a65151f1e
Rao Fahad <raofahad046@gmail.com>
2021-05-06 11:53:00 +0500

    created a GlobalProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useContext } from "react";
 import "./App.css";
+import Decrement from "./components/Decrement";
+import Increment from "./components/Increment";
+import Reset from "./components/Reset";
+import { GlobalProvider } from "./GlobalProvider";
+import { myContext } from "./myContext";
 
 function App() {
+  const { initial_state } = useContext(myContext);
   return (
-    <div className="App">
-      <h1>Counter App With Context Api</h1>
-    </div>
+    <GlobalProvider>
+      <div className="App">
+        <h1>Counter App With Context Api</h1>
+        {/* <h3>{initial_state.count}</h3> */}
+        <Increment />
+        <Decrement />
+        <Reset />
+      </div>
+    </GlobalProvider>
   );
 }
 
---
+++ src/GlobalProvider.js
@@ -0,0 +1,29 @@
+import React, { useContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+import { myContext } from "./myContext";
+import { DECREMENT, INCREMENT, RESET } from "./types";
+
+export const GlobalProvider = ({ children }) => {
+  const { initial_state } = useContext(myContext);
+
+  const [state, dispatch] = useReducer(AppReducer, initial_state);
+
+  const incrementCount = () => dispatch({ type: INCREMENT });
+
+  const decrementCount = () => dispatch({ type: DECREMENT });
+
+  const resetCount = () => dispatch({ type: RESET });
+
+  return (
+    <myContext.Provider
+      value={{
+        count: state.count,
+        incrementCount,
+        decrementCount,
+        resetCount,
+      }}
+    >
+      {children}
+    </myContext.Provider>
+  );
+};
